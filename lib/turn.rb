c6555f5bebd37181cbbeede4b9ab0d78f053325d
Tyler Hunt <tyhunt04@gmail.com>
2019-02-09 18:24:21 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -7,7 +7,7 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def valid_move?(board, postition)
+def valid_move?(board, index)
   def position_taken?(board, position)
     if board[position] == " " || board[position] == "" || board[position] == nil
       false
@@ -24,7 +24,7 @@ def valid_move?(board, postition)
     end
   end
 
-  if (position_taken?(board, postition)) == false && (on_board?(index)) == true
+  if (position_taken?(board, index)) == false && (on_board?(index)) == true
     true
   else
     false
