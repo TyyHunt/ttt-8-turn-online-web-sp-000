e86eddb1c07a5d3f6635137b9314c353996ab314
Tyler Hunt <tyhunt04@gmail.com>
2019-02-09 18:37:01 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -46,7 +46,7 @@ def turn(board)
   num = gets.chomp
   index= input_to_index(num)
   if (valid_move?(board,index)) == true
-    move(board, index)
+    move(board, index, value)
     display_board(board)
   else
     turn(board)
