5838b982a668f45009fe5f8c66af45bbcad48fef
Tyler Hunt <tyhunt04@gmail.com>
2019-02-09 17:25:35 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,7 +6,7 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
-
+input_to_index = gets.strip.to_i - 1
 def valid_move?(board, index)
   def position_taken?(array, ind)
     if array[ind] == " " || array[ind] == "" || array[ind] == nil
