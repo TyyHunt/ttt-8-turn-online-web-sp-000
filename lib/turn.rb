667f9a367d2bc357b43a6d5ab6b3d5cbdc0b0f57
Tyler Hunt <tyhunt04@gmail.com>
2019-02-09 17:19:40 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,32 @@
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def valid_move?(board, index)
+  def position_taken?(array, ind)
+    if array[ind] == " " || array[ind] == "" || array[ind] == nil
+      false
+    else
+      true
+    end
+  end
+
+  def valid_move?(num)
+    if num.between?(0, 8) == true
+      true
+    else
+      false
+    end
+  end
+
+  if (position_taken?(array, ind)) == false && (valid_move?(index)) == true
+    true
+  else
+    false
+  end
+end
